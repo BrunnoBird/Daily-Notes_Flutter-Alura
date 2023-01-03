9d728dc4f659cc4c7bcd9c9bc070555b8cdbc6d1
“brunnoBird” <bird.tec.gonzalez@gmail.com>
2023-01-03 18:02:54 -0300

    first commit

---
+++ lib/services/http_interceptors.dart
@@ -0,0 +1,29 @@
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
+
+class HttpInterceptor implements InterceptorContract {
+  Logger logger = Logger();
+
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    logger.v(
+      "Requisição para: ${data.baseUrl}\nCabecalhos: ${data.headers}\nCorpo ${data.body}",
+    );
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    if (data.statusCode ~/ 100 == 2) {
+      logger.i(
+        "Resposta de: ${data.url}\nStatus Code: ${data.statusCode}\nCabecalhos: ${data.headers}\nCorpo ${data.body}",
+      );
+    } else {
+      logger.e(
+        "Resposta de: ${data.url}\nStatus Code: ${data.statusCode}\nCabecalhos: ${data.headers}\nCorpo ${data.body}",
+      );
+    }
+
+    return data;
+  }
+}
---
+++ lib/services/journal_service.dart
@@ -0,0 +1,59 @@
+import 'dart:convert';
+
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
+
+class JournalService {
+  static const String url = 'http://10.0.0.182:3000/';
+  static const String resouce = "journals/";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [HttpInterceptor()]);
+
+  String getUrl() {
+    return "$url$resouce";
+  }
+
+  Future<bool> register(Journal journal) async {
+    //convertando meu objeto para json depois de transforma-lo para MAP.
+    String jsonJournal = json.encode(journal.toMap());
+
+    http.Response response = await client.post(
+      Uri.parse(getUrl()),
+      headers: {'Content-type': 'application/json'},
+      body: jsonJournal,
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<List<Journal>> getAll() async {
+    http.Response response = await client.get(
+      Uri.parse(getUrl()),
+    );
+    if (response.statusCode != 200) {
+      throw Exception();
+    }
+
+    List<Journal> list = [];
+    //Transformo de String para JSON
+    List<dynamic> listDynamic = json.decode(response.body);
+
+    //1- Itero para cada list do JSON.
+    //2- Transformo cada item da lista do JSON em um MAP usndo o fromMap.
+    //3- Journal.fromMap -> crio um objeto Journal para adicionar na lista final.
+    //4 - Retorno a justa de Objetos Journal para a tela.
+
+    for (var jsonMap in listDynamic) {
+      list.add(Journal.fromMap(jsonMap));
+    }
+
+    return list;
+  }
+}
